643f70e52212962a32de7e4f402b8e4aa387751d
JadonHill36 <hilljad@mail.gvsu.edu>
2024-10-05 17:47:38 -0400

    Update account.py

--- account.py
+++ account.py
@@ -17,7 +17,20 @@ class Account:
     def add_hundred(self):
         self._balance += 100
 
+
+    def place_bet(self, bet_amount):
+        self.invalid_bet_message = ''
+        if bet_amount <= self._balance:
+            self._balance -= bet_amount
+        else:
+            self.invalid_bet_message = 'You cannot bet more than you have!'
+
+
+
     def __str__(self):
         return '$' + str(self.balance)
+
+
+
 
 
