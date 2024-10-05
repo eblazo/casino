d933d741c500b63be00ec9eb24922d038bb6d38c
JadonHill36 <hilljad@mail.gvsu.edu>
2024-10-05 17:52:41 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+import time
 from account import Account
 
 # Initialize Pygame
@@ -47,14 +49,23 @@ add_button3_text = font.render("$100", True, WHITE)
 back_button = pygame.Rect(0, 0, 120, 40)
 back_button_text = small_font.render("Back", True, WHITE)
 
+# Create bet button
+bet_button = pygame.Rect(360, 500, 60, 50)
+spin_button = pygame.Rect(460, 500, 100, 50)
+
 # Sets current screen to menu
 current_screen = 0
+bet_text = ''
+invalid_bet_message = ''
+message_rect = pygame.Rect(200, 450, 400, 50)
+
+# Create balance button outside the loop
+balance_button = pygame.Rect(680, 0, 120, 40)
 
 # Main loop
 running = True
 while running:
-    # Creates balance button
-    balance_button = pygame.Rect(680, 0, 120, 40)
+    # Update balance button text every iteration
     balance_button_text = small_font.render(str(account), True, WHITE)
 
     # Handle events
@@ -62,35 +73,33 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # handles mouse clicks on various buttons
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 mouse_pos = pygame.mouse.get_pos()
                 if button1.collidepoint(mouse_pos) and current_screen == 4:
                     account.add_twenty()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                if button2.collidepoint(mouse_pos) and current_screen == 4:
+                elif button2.collidepoint(mouse_pos) and current_screen == 4:
                     account.add_fifty()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                if button3.collidepoint(mouse_pos) and current_screen == 4:
+                elif button3.collidepoint(mouse_pos) and current_screen == 4:
                     account.add_hundred()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                if back_button.collidepoint(mouse_pos):
+                elif back_button.collidepoint(mouse_pos):
                     current_screen = 0
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                if balance_button.collidepoint(mouse_pos):
+                elif balance_button.collidepoint(mouse_pos):
                     current_screen = 4
+
+                if spin_button.collidepoint(mouse_pos) and current_screen == 1:
+                    # makes sure bet is not subtracted from balance if bet is above 20
+                    if 0 < int(bet_text) <= 20:
+                        account.place_bet(int(bet_text))
+
+        # types and backspaces in the bet amount rectangle
+        if current_screen == 1:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    bet_text = bet_text[:-1]
+                elif event.unicode.isdigit() and len(bet_text) < 2:
+                    bet_text += event.unicode
 
     mouse_pos = pygame.mouse.get_pos()
     mouse_pressed = pygame.mouse.get_pressed()
@@ -141,6 +150,35 @@ while running:
         # Account display / button
         pygame.draw.rect(screen, BLUE, balance_button)
         screen.blit(balance_button_text, (balance_button.x + 30, balance_button.y + 10))
+
+        # bet amount rectangle
+        pygame.draw.rect(screen, BLUE, bet_button, 2)
+        text_surface = font.render(bet_text, True, BLACK)
+        screen.blit(text_surface, (bet_button.x + 10, bet_button.y + 10))
+
+        # place bet text
+        place_bet_text = font.render('Place bet: $', True, BLACK)
+        screen.blit(place_bet_text, (bet_button.x - 190, bet_button.y + 10))
+
+        # spin button
+        pygame.draw.rect(screen, BLUE, spin_button)
+        spin_button_text = font.render('SPIN', True, WHITE)
+        screen.blit(spin_button_text, (spin_button.x + 10, spin_button.y + 10))
+
+        # make checks for bet amounts
+        if spin_button.collidepoint(mouse_pos) and mouse_pressed[0]:
+
+            if int(bet_text) > 20 or int(bet_text) < 1:
+                invalid_bet_message = 'Bet must be between $1 and $20!'
+            elif int(bet_text) > account.balance:
+                invalid_bet_message = account.invalid_bet_message
+            else:
+                invalid_bet_message = ''
+
+        # display message if bet is out of range or more than the total balance
+        if invalid_bet_message:
+            message_surface = small_font.render(invalid_bet_message, True, BLACK)
+            screen.blit(message_surface, message_rect)
 
     # Roulette screen
     elif current_screen == 2:
