6ebcc0c8debbf8b8b80c8c637637327c948387df
Spencer <sdlincicum@gmail.com>
2024-10-24 22:43:38 -0400

    Added the card class to the blackjack file. It probobly doesn't need it's own file.

--- blackjack2.py
+++ blackjack2.py
@@ -1,5 +1,52 @@
 from random import shuffle
-from card import Card
+
+
+class Card:
+    def __init__(self, number: int, suit: int, face_up=True):
+        self.__number = number
+        self.__suit = suit
+        self.__face_up = face_up
+
+    @property
+    def number(self):
+        return self.__number
+
+    @property
+    def suit(self):
+        return self.__suit
+
+    @property
+    def face_up(self):
+        return self.__face_up
+
+    @face_up.setter
+    def face_up(self, face_up: bool):
+        self.__face_up = face_up
+
+    def named(self):
+        values = {1: 'Ace',
+                  2: '2',
+                  3: '3',
+                  4: '4',
+                  5: '5',
+                  6: '6',
+                  7: '7',
+                  8: '8',
+                  9: '9',
+                  10: '10',
+                  11: 'Jack',
+                  12: 'Queen',
+                  13: 'King'
+                  }
+
+        suits = {1: 'Clubs',
+                 2: 'Diamonds',
+                 3: 'Hearts',
+                 4: 'Spades'
+                 }
+        if self.face_up:
+            return values.get(self.number), suits.get(self.suit)
+        return "a faced down card"
 
 
 class Player:
@@ -95,7 +142,7 @@ class Table:
     def player_win(self, num_code: int):
         if not isinstance(num_code, int):
             raise TypeError('The player_win codes are integers')
-        if not num_code in [-1, 0, 1, 2]:
+        if not num_code not in [-1, 0, 1, 2]:
             raise ValueError('The player_win codes are from -1 to 2')
 
         self.__player_win = num_code
