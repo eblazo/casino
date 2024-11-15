bb41fb0436fd6e80c2f594e0cb005d9345a9e65c
eblazo <blazoe@mail.gvsu.edu>
2024-11-14 22:03:29 -0500

    adds empty unittest file

---
+++ casino_test.py
@@ -0,0 +1,15 @@
+import unittest
+from BlackJack import Table, Player, Hand, Deck, Card
+from account import Account
+from Slots import Slots
+from Roulette import Roulette
+
+
+class TestYourClassOrFunction(unittest.TestCase):
+
+    def example(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
