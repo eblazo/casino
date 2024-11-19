22711c99b1a05f817c709e0fd0466d97436aa46f
eblazo <blazoe@mail.gvsu.edu>
2024-11-19 17:48:54 -0500

    adds two unit tests

--- casino_test.py
+++ casino_test.py
@@ -1,14 +1,31 @@
 import unittest
-from BlackJack import Table, Player, Hand, Deck, Card
+from BlackJack import Table
 from account import Account
-from Slots import Slots
 from Roulette import Roulette
 
 
 class TestYourClassOrFunction(unittest.TestCase):
 
-    def example(self):
-        pass
+    def test_roulette_update_balance(self):
+        # Checks that the account balance is updated correctly when the player
+        # wins in blackjack.
+        # Requirement  7
+        r = Roulette()
+        a = Account()
+        r.bet_position("0", a)
+        r._landing_number = "0"
+        winnings = r.payout()
+        a.deposit(winnings)
+        self.assertEqual(a.balance, 275)
+
+    def test_cards_delt_after_bet(self):
+        # Checks that the user is dealt two cards after they bet.
+        # Requirement 16
+        a = Account()
+        b = Table(a)
+        b.deal()
+        self.assertEqual(len(b.player.hand.cards), 2)
+        self.assertEqual(len(b.dealer.hand.cards), 2)
 
 
 if __name__ == '__main__':
